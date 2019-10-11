be6b55b5c86cb33594728ff7946e76296a5f9001
Bean Yen <allen20046@gmail.com>
2019-10-11 13:43:04 +0800

    add uwsgi autostart module, modify settings

--- fii_ai_api/settings.py
+++ fii_ai_api/settings.py
@@ -88,8 +88,14 @@ WSGI_APPLICATION = 'fii_ai_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
-    'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3')},
-    # 'fii-ai': {'ENGINE': 'django.db.backends.mysql', 'NAME': ''},
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '10.124.131.87',
+        'PORT': 3306,
+        'USER': 'api',
+        'PASSWORD': 'Develop123!@#',
+        'NAME': 'init',
+    }
 }
 
 
---
+++ uwsgi_autostart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# chkconfig: 2345 10 90
+# description: myservice ....
+/usr/bin/uwsgi --ini /opt/api-server/uwsgi.ini
